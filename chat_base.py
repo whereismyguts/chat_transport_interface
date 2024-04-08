7e3159124648aebef69d8a43b2e8cd4949000cbd
whereismyguts <whereismyguts@gmail.com>
2024-04-08 17:18:11 +0300

    refactor

--- chat_base.py
+++ chat_base.py
@@ -3,16 +3,14 @@ import discord
 import asyncio
 from aiogram import Bot, Dispatcher, Router, types
 from aiogram.enums import ParseMode
-from aiogram.filters import CommandStart
-from aiogram.types import Message
-from aiogram.utils.markdown import hbold
 
-import asyncio
 
 # TODO move to separate files
+# TODO add logging
 
 class ChatTransport(ABC):
     def __init__(self, chat_id):
+        self.chat_id = chat_id
         self.handlers = {}
 
     def add_handler(self, event, handler):
@@ -27,25 +25,25 @@ class ChatTransport(ABC):
         pass
 
 
-
 class ChatTransportDiscord(ChatTransport):
     def __init__(self, token, channel_id):
         super().__init__(channel_id)
-        
-        
-        # intents = discord.Intents.default()
-        # intents.message_content = True
-        # intents.messages = True
-        intents = discord.Intents.all()
-        
+        #  intents = discord.Intents.all()
+        intents = discord.Intents(
+            message_content=True,
+            messages=True,
+            guilds=True,
+        )
         self.client = discord.Client(intents=intents)
         self.channel_id = channel_id
-        
         self.token = token
 
     async def send_message(self, msg):
-        channel = self.client.get_channel(int(self.channel_id))  
-        await channel.send(msg)
+        try:
+            channel = self.client.get_channel(int(self.channel_id))
+            await channel.send(msg)
+        except Exception as e:
+            print(f"Failed to send message on Discord: {e}")
 
     async def run(self):
         @self.client.event
@@ -57,13 +55,11 @@ class ChatTransportDiscord(ChatTransport):
             if message.author == self.client.user:
                 return
 
-            handler = self.handlers.get('message')
-            
+            handler = self.handlers.get('message')  # TODO event types
             if handler:
                 await handler(message.content)
 
         await self.client.start(self.token)
-
 
 
 class ChatTransportTelegram(ChatTransport):
@@ -74,26 +70,28 @@ class ChatTransportTelegram(ChatTransport):
 
     async def send_message(self, msg_text):
         # todo add msg ojbect to proper reply
-        await self.bot.send_message(chat_id=self.chat_id, text=msg_text)
+        try:
+            await self.bot.send_message(chat_id=self.chat_id, text=msg_text)
+        except Exception as e:
+            print(f"Failed to send message on Telegram: {e}")
 
     async def run(self):
         @self.dp.message()
         async def echo_handler(message: types.Message) -> None:
-            handler = self.handlers.get('message')
+            handler = self.handlers.get('message')  # #TODO event types
             if handler:
                 await handler(message.text)
 
         await self.dp.start_polling(self.bot, skip_updates=True)
-        
 
 
 class BusinessLogicController:
     def __init__(self, transport):
         self.transport = transport
-        self.transport.add_handler('message', self.handle_message) # todo add more handlers (commands, files, voices)
+        self.transport.add_handler('message', self.handle_message)  # TODO handler types
 
     async def handle_message(self, msg):
-        print(f'BusinessLogicController: message received: {msg}')
+        print(f'BusinessLogicController ({self.transport}): message received: {msg}')
         response = f'Hi! Your message was received: "{msg}"'
         await self.transport.send_message(response)
 
@@ -105,18 +103,18 @@ if __name__ == "__main__":
     import os
     from dotenv import load_dotenv
     load_dotenv()
-    
+
     telegram_bot_token = os.getenv('telegram_bot_token')
     telegram_chat_id = os.getenv('telegram_chat_id')
-    
+
     discord_bot_token = os.getenv('discord_bot_token')
     discord_channel_id = os.getenv('discord_channel_id')
 
+    # discord
+    # transport = ChatTransportDiscord(discord_bot_token, discord_channel_id)
 
-        
-    transport = ChatTransportDiscord(discord_bot_token, discord_channel_id)
-    # transport = ChatTransportTelegram(telegram_bot_token, telegram_chat_id)
+    # telegram
+    transport = ChatTransportTelegram(telegram_bot_token, telegram_chat_id)
 
     bot = BusinessLogicController(transport)
     asyncio.run(bot.run())
-
